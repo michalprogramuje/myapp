d9ede1ff9036ac0ade441420de23edcb812e5f60
mzlotorzynski <zloty667@gmail.com>
2020-01-08 15:15:25 +0100

    40

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'franek', age:28 },
+      { name: 'antek', age: 29},
+      { name: 'pani ania', age: 26}
+    ]
+  }
+  switchNameHandler = () => {
+    console.log('Was clicked');
+  }
   render() {
     return (
       <div className="App">
         <h1>No witam witam</h1>
-        <p>Serdecznie</p>
-        <Person name = 'franek' />
-        <Person name = 'antek'>WITAM</Person>
-        <Person name = 'pani ania' />
+        <p>Serdecznia</p>
+        <button onClick={this.switchNameHandler}>Switch</button>
+        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
+        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].name}>WITAM</Person>
+        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
       </div>
     );
   // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Hi I\'m React App'))
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react'
 const person = (props) => {
   return (
     <div>
-      <p>To jest {props.name} i ma {Math.floor(Math.random() * 30)} lat</p>
+      <p>To jest {props.name} i ma {props.age} lat</p>
       <p>{props.children}</p>
     </div>
 )
