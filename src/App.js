93b86f70ad049221ec2c04424ac65f7e625c0e7d
mzlotorzynski <zloty667@gmail.com>
2020-02-04 15:13:53 +0100

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   state = {
@@ -85,6 +85,7 @@ class App extends Component {
 
 
     return (
+      <StyleRoot>
       <div className="App">
         <h1>No witam witam</h1>
         <p className={classes.join(' ')}>Serdecznie</p>
@@ -93,6 +94,7 @@ class App extends Component {
         onClick={this.togglePersonsHandler}>Show/Hide</button>
         {persons}
       </div>
+      </StyleRoot>
     );
   }
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,13 @@ import './Person.css';
 import Radium from 'radium';
 
 const person = (props) => {
+  const style = {
+    '@media(min-width: 500px)': {
+      width: '450px'
+    }
+  };
   return (
-    <div className = 'Person'>
+    <div className = 'Person' style={style}>
       <p onClick ={props.click}>To jest {props.name} i ma {props.age} lat</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value = {props.name} />
