92f4dcd3ca2761296fd7d4af4fd51e947ea4208d
michalprogramuje <zloty667@gmail.com>
2020-03-01 11:16:46 +0100

    ref

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,13 +1,16 @@
-import React, { useEffect }  from 'react';
+import React, { useEffect, useRef }  from 'react';
 import classes from "./Cockpit.css";
 
 const Cockpit = (props) => {
 
+    const toggleBtnRef = useRef(null);
+
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
-        setTimeout(() => {
-        alert('Saved data to cloud (nope)')
-        }, 1000);
+        // setTimeout(() => {
+        // alert('Saved data to cloud (nope)')
+        // }, 1000);
+        toggleBtnRef.current.click();
         return () => {
             console.log('[Cockpit.js] cleanup work in useEffect')
         }
@@ -38,6 +41,7 @@ const Cockpit = (props) => {
             <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>Serdecznie</p>
             <button
+                ref = {toggleBtnRef}
                 className = {btnClass}
                 onClick={props.clicked}>Show/Hide</button>
         </div>
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,41 @@
 import React, { Component, Fragment } from 'react';
 import classes from "./Person.css";
-import withClass from '../../../hoc/WithClass'
+import withClass from '../../../hoc/WithClass';
+import PropTypes from 'prop-types';
 
 class Person extends Component {
+
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...');
             return (
                 <Fragment>
                 <p onClick ={this.props.click}>To jest {this.props.name} i ma {this.props.age} lat</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value = {this.props.name} />
+                <input 
+                type= "text"
+                // ref = {(inputEl) => {this.inputElement = inputEl}}
+                ref = {this.inputElementRef}
+                onChange = {this.props.changed} 
+                value = {this.props.name} />
                 </Fragment>
         );
 }}
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
+
+
 export default withClass(Person, classes.Person);
